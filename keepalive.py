a847d33ed3439d7882f7583ba52932bc04e58425
JoelHer <JoelHerbst07@gmail.com>
2023-01-14 23:55:05 +0100

    Able to get important information about users

--- keepalive.py
+++ keepalive.py
@@ -1,11 +1,30 @@
-from flask import Flask, jsonify
+from flask import Flask, request, jsonify
 from threading import Thread
-
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/get-user-info', methods=['GET'])
 def home():
-    return jsonify('Hi iHouq')
+  if request.headers.get('request-user-info', "none") == "none":
+    return jsonify({
+      "success": False,
+      "message": "Insufficient Headers: request-user-info missing"
+    })
+  else:
+    try:
+      user = request.headers.get('request-user-info', "none")
+      print(user)
+      return jsonify({
+      "success": True,
+      "user": {
+        "name": request.headers.get('request-user-info', "none"),
+        "avatar": "https://cdn.discord.com/shitAvatar",
+        "status": "online"
+      }
+    })
+    except:
+      return jsonify({ 
+        "success": False
+      })
 
 def run():
   app.run(host='0.0.0.0',port=8080)
